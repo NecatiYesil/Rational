7459143a26a47407c6f3444837d04a660a49ff1d
David Serrano <davidserrano@cis.vvc.edu>
2018-11-01 11:03:10 -0700

    update

--- rational.h
+++ rational.h
@@ -1,1 +1,10 @@
-#incldue <iostream>
+#include <iostream>
+
+class Rational{
+private:
+    int numerator, denominator;
+public:
+    Rational(int _numerator, int _denominator);
+    string print();
+    string set(int _numerator, int _denominator);
+}
