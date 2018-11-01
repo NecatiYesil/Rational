86cccc63f5bc6f93b5dba05bc14e5bda5f7a8330
David Serrano <davidserrano@cis.vvc.edu>
2018-11-01 11:32:49 -0700

    update 2

--- rational.h
+++ rational.h
@@ -4,6 +4,35 @@ class Rational{
 private:
     int numerator, denominator;
 public:
-    Rational(int _numerator, int _denominator);
-    void  set(int _numerator, int _denominator);
+    void set(int _numerator, int _denominator);
+    Rational add(Rational R2); 
 };
+
+
+
+void Rational::set(int _numerator, int _denominator)
+{
+    numerator = _numerator;
+    denominator = _denominator;
+}
+
+Rational Rational::add(Rational R2)
+{
+    int N3, D3;
+    N3 = (numerator*R2.denominator) + (R2.numerator*denominator);
+    D3 = denominator * R2.denominator;
+    Rational new_rational;
+    new_rational.set( N3, D3);
+    return new_rational;
+}
+
+
+int main()
+{
+    Rational Rational1;
+    Rational1.set(3, 5);
+    Rational Rational2;
+    Rational2.set(2,6);
+    Rational Rational3 = Rational1.add(Rational2);
+
+}
